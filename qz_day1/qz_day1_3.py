824d197ea94639eaeaa9bbbba417cc61983633c0
任行 <297096995@qq.com>
2017-07-27 21:43:27 +0800

    学习笔记-day1

---
+++ qz_day1/qz_day1_3.py
@@ -0,0 +1,15 @@
+# /user/bin/evn/python
+# -*- coding: utf-8 -*-
+# Author:Ren Hang
+
+# 导入自己写的模块 qz_day1_2
+
+import qz_day1_2
+
+print("hello world!")
+
+"""
+打印结果:
+1 hello world
+hello world!
+"""
